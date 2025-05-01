c5d7cec8e189ad6b705348964d674f9aa17cbb59
BoudaDols <dolsombouda@gmail.com>
2025-05-01 00:35:44 -0400

    controller delete

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/', function(){
+    return response()->json(['message' => 'Welcome to the API CarCo!']);
+});
