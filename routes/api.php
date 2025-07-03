bfa0d928b0158c5830ad26f2eaae1a455977087e
BoudaDols <dolsombouda@gmail.com>
2025-07-03 13:21:01 -0400

    Modification de api.php pour test

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,11 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
         Route::post('/logout', [AuthController::class, 'logout']);
+
+    });
+});
+
+
         Route::post('/car/create', [CarController::class, 'addCar']);
         Route::get('/cars', [CarController::class, 'getCars']);
         Route::get('/car/{id}', [CarController::class, 'getCarById']);
@@ -43,9 +48,6 @@ Route::prefix('auth')->group(function () {
         Route::put('/brand/{id}', [BrandController::class, 'updateBrand']);
         Route::delete('/brand/{id}', [BrandController::class, 'deleteBrand']);
         Route::get('/brands/cars', [BrandController::class, 'getCarsByBrand']);
-
-    });
-});
 
 Route::get('/', function(){
     return response()->json(['message' => 'Welcome to the API CarCo!']);
