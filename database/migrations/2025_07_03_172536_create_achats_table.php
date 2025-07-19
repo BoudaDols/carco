64c230acf71de6bad5bbbfb3f142fe8bf70f5f59
BoudaDols <dolsombouda@gmail.com>
2025-07-18 20:06:12 -0400

    creation du moule client et ajout des champs pour Achat

--- database/migrations/2025_07_03_172536_create_achats_table.php
+++ database/migrations/2025_07_03_172536_create_achats_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('achats', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('car_id');
+            $table->unsignedBigInteger('client_id');
             $table->timestamps();
+
+
+            //Declaration de clés etrangères avec Car
+            $table->foreign('car_id')
+                    ->references('id')
+                    ->on('cars')
+                    ->onDelete('cascade');
+
+
+            //Declaration de clés etrangères avec Client
+            $table->foreign('client_id')
+                    ->references('id')
+                    ->on('clients')
+                    ->onDelete('cascade');
         });
     }
 
