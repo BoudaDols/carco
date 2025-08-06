32b336db9461545e04c9a6df9bff3f4c2e182810
BoudaDols <dolsombouda@gmail.com>
2025-08-06 16:30:53 -0400

    Optimisation du module client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,117 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ClientRequest;
+use App\Models\Client;
 
 class ClientController extends Controller
 {
+    public function addClient(ClientRequest $request)
+    {
+        $client = Client::create($request->validated());
 
-    /*
-    *   Add a client
-    *   @param Request $request
-    *   @return Response
-    */
-    public function addClient(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'dateNaissance' => 'required|string|max:10',
-            'sexe' => 'required|string|max:1',
-            'domaineP' => 'required|string|max:25',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $client = new Client();
-        $client->name = $request->input('name');
-        $client->dateNaissance = $request->input('dateNaissance');
-        $client->sexe = $request->input('sexe');
-        $client->domaineP = $request->input('domaineP');
-        $client->save();
-
-        return response()->json(['message' => 'Client added successfully'], 200);
+        return response()->json($client, 201);
     }
 
-
-    /*
-    *   Get all clients
-    *   @return Response
-    */
-    public function getClients(){
-        $clients = Client::all();
-        return response()->json($clients, 200);
+    public function getClients()
+    {
+        return response()->json(Client::all());
     }
 
-
-    /*
-    *   Get a client by name
-    *   @param Request $request
-    *   @return Response
-    */
-    public function getClientByName(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|exists:clients,name'
-        ]);
-
-        if($validator->fails()){
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $client = Client::where('name', $request->input('name'))->get();
-
-        return response()->json($client, 200);
+    public function getClientById(Client $client)
+    {
+        return response()->json($client);
     }
 
+    public function updateClient(ClientRequest $request, Client $client)
+    {
+        $client->update($request->validated());
 
-
-    /*
-    *   Update a client
-    *   @param Request $request
-    *   @return Response
-    */
-    public function updateClient(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|exists:clients,name',
-            'dateNaissance' => 'required|string|max:10',
-            'sexe' => 'required|string|max:1',
-            'domaineP' => 'required|string|max:25',
-        ]);
-
-        if($validator->fails()){
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $client = Client::where('name', $request->input('name'))->first();
-
-        $client->dateNaissance = $request->input('dateNaissance');
-        $client->sexe = $request->input('sexe');
-        $client->domaineP = $request->input('domaineP');
-        $client->save();
-
-        return response()->json(['message' => 'Client updated successfully'], 200);
+        return response()->json($client);
     }
 
-
-
-    /*
-    *   Delete a client
-    *   @param Request $request
-    *   @return Response
-    */
-    public function deleteClient(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|exists:clients,name'
-        ]);
-
-        if($validator->fails()){
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $client = Client::where('name', $request->input('name'))->first();
+    public function deleteClient(Client $client)
+    {
         $client->delete();
 
-        return response()->json(['message' => 'Client deleted successfully'], 200);
+        return response()->json(null, 204);
     }
-    
 }
