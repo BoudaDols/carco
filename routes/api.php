58dcffe31f12332126869b0c22d6d16d473d31ed
BoudaDols <dolsombouda@gmail.com>
2025-08-06 22:54:40 -0400

    Finalisation V1

--- routes/api.php
+++ routes/api.php
@@ -17,45 +17,47 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
 
     });
+
+    Route::prefix('cars')->group(function () {
+        Route::post('/', [CarController::class, 'addCar']);
+        Route::get('/', [CarController::class, 'getCars']);
+        Route::get('/{car}', [CarController::class, 'getCarById']);
+        Route::put('/{car}', [CarController::class, 'updateCar']);
+        Route::delete('/{car}', [CarController::class, 'deleteCar']);
+    });
+
+    Route::prefix('categories')->group(function () {
+        Route::post('/', [CategorieController::class, 'addCategorie']);
+        Route::get('/', [CategorieController::class, 'getCategories']);
+        Route::get('/{categorie}', [CategorieController::class, 'getCategorieById']);
+        Route::put('/{categorie}', [CategorieController::class, 'updateCategorie']);
+        Route::delete('/{categorie}', [CategorieController::class, 'deleteCategorie']);
+        Route::get('/{categorie}/cars', [CategorieController::class, 'getCarsByCategorie']);
+    });
+
+    Route::prefix('brands')->group(function () {
+        Route::post('/', [BrandController::class, 'addBrand']);
+        Route::get('/', [BrandController::class, 'getBrands']);
+        Route::get('/{brand}', [BrandController::class, 'getBrandById']);
+        Route::put('/{brand}', [BrandController::class, 'updateBrand']);
+        Route::delete('/{brand}', [BrandController::class, 'deleteBrand']);
+        Route::get('/{brand}/cars', [BrandController::class, 'getCarsByBrand']);
+    });
+
+    Route::post('/achat', [AchatController::class, 'addAchat']);
+    Route::get('/achats', [AchatController::class, 'getAchats']);
+    Route::get('/achat/{achat}', [AchatController::class, 'getAchatById']);
+    Route::put('/achat/{achat}', [AchatController::class, 'updateAchat']);
+    Route::delete('/achat/{achat}', [AchatController::class, 'deleteAchat']);
+
+    Route::post('/client', [ClientController::class, 'addClient']);
+    Route::get('/clients', [ClientController::class, 'getClients']);
+    Route::get('/client/{client}', [ClientController::class, 'getClientById']);
+    Route::put('/client/{client}', [ClientController::class, 'updateClient']);
+    Route::delete('/client/{client}', [ClientController::class, 'deleteClient']);
 });
 
-Route::prefix('cars')->group(function () {
-    Route::post('/', [CarController::class, 'addCar']);
-    Route::get('/', [CarController::class, 'getCars']);
-    Route::get('/{car}', [CarController::class, 'getCarById']);
-    Route::put('/{car}', [CarController::class, 'updateCar']);
-    Route::delete('/{car}', [CarController::class, 'deleteCar']);
-});
 
-Route::prefix('categories')->group(function () {
-    Route::post('/', [CategorieController::class, 'addCategorie']);
-    Route::get('/', [CategorieController::class, 'getCategories']);
-    Route::get('/{categorie}', [CategorieController::class, 'getCategorieById']);
-    Route::put('/{categorie}', [CategorieController::class, 'updateCategorie']);
-    Route::delete('/{categorie}', [CategorieController::class, 'deleteCategorie']);
-    Route::get('/{categorie}/cars', [CategorieController::class, 'getCarsByCategorie']);
-});
-
-Route::prefix('brands')->group(function () {
-    Route::post('/', [BrandController::class, 'addBrand']);
-    Route::get('/', [BrandController::class, 'getBrands']);
-    Route::get('/{brand}', [BrandController::class, 'getBrandById']);
-    Route::put('/{brand}', [BrandController::class, 'updateBrand']);
-    Route::delete('/{brand}', [BrandController::class, 'deleteBrand']);
-    Route::get('/{brand}/cars', [BrandController::class, 'getCarsByBrand']);
-});
-
-Route::post('/achat', [AchatController::class, 'addAchat']);
-Route::get('/achats', [AchatController::class, 'getAchats']);
-Route::get('/achat/{achat}', [AchatController::class, 'getAchatById']);
-Route::put('/achat/{achat}', [AchatController::class, 'updateAchat']);
-Route::delete('/achat/{achat}', [AchatController::class, 'deleteAchat']);
-
-Route::post('/client', [ClientController::class, 'addClient']);
-Route::get('/clients', [ClientController::class, 'getClients']);
-Route::get('/client/{client}', [ClientController::class, 'getClientById']);
-Route::put('/client/{client}', [ClientController::class, 'updateClient']);
-Route::delete('/client/{client}', [ClientController::class, 'deleteClient']);
 
 Route::get('/', function () {
     return response()->json(['message' => 'Welcome to the API CarCo!']);
